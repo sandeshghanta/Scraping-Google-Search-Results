05e78fba7224beca9cf96ccb5f69d8c794d1dec8
Tony Thomas <01tonythomas@gmail.com>
2016-08-14 15:43:11 +0530

    Updated the web scraping code to look pretty!

--- scraping google.py
+++ scraping google.py
@@ -1,20 +1,16 @@
 import requests, re
 from bs4 import BeautifulSoup
+
 url = 'http://www.google.com/search'
-web= raw_input('Enter your search parameter')
-payload = { 'q' : web }
+query = raw_input('Enter your search parameter: ')
+payload = { 'q' : query }
+
 my_headers = { 'User-agent' : 'Mozilla/11.0' }
 r = requests.get( url, params = payload, headers = my_headers )
+
 soup = BeautifulSoup( r.text, 'html.parser' )
 h3tags = soup.find_all( 'h3', class_='r' )
+
 for h3 in h3tags:
-    try:
-	print
-	
-        print (re.search('url\?q=(.+?)\&sa', h3.a['href']).group(1))
-	
-    except:
-        continue
-
-
-
+  if h3:
+    print h3.a['href'].split('q=')[1].split('/')[2]
